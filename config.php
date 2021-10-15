1893ed0b55e13c61553c80ece4f63675df53f98d
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-10-15 15:57:38 +0100

    login fixed

--- config.php
+++ config.php
@@ -10,6 +10,6 @@ $sql_db_pass = "";
 $sql_db_name = "teago";
 
 // Site URL
-$site_url = "http://localhost/Teago"; // e.g (http://example.com)
+$site_url = "http://localhost/software/teago"; // e.g (http://example.com)
  
  ?>
